f916c8ae60243ed467f66c037d0e5f04e58fbf8c
swaroopc236 <swaroopc236@gmail.com>
2021-02-11 00:01:10 +0530

    updated Schema, error codes, models folder

---
+++ backend/api/models/meme.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const memeSchema = new Schema({
+    id: Number,
+    memeOwner: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    memeCaption: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    memeUrl: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    created_at: String,
+    updated_at: String
+})
+
+const Meme = mongoose.model('Meme', memeSchema)
+module.exports = Meme
---
+++ backend/api/routes/memes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+let Meme = require('../models/meme')
+
+let count = 1
+
+router.get('/',(req,res,next)=>{
+    // return res.status(200).json({
+    //     'message': 'Viewing all memes'
+    // })
+    Meme.find().sort({created_at: -1}).limit(100)
+        .then(memes => res.json(memes))
+        .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.post('/',(req,res,next)=>{
+    // return res.status(201).json({
+    //     'memeOwner':req.body.memeOwner,
+    //     'memeCaption': req.body.memeCaption,
+    //     'memeUrl': req.body.memeUrl,
+    //     'message': 'Meme Created'
+    // })
+    var newMeme = new Meme({
+        id: count,
+        memeOwner: req.body.memeOwner,
+        memeCaption: req.body.memeCaption,
+        memeUrl: req.body.memeUrl,
+        created_at: Date.now().toString()
+    })
+    newMeme.save()
+        .then(() => res.json('Meme added'))
+        .catch(err => res.status(400).json('Error: ' + err))
+
+    count += 1;
+})
+
+module.exports= router
